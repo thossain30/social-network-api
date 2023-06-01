ba1b03d775615cbe6089ed5b83665044193169b2
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-05-31 22:59:41 -0400

    added user routes and began other routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+    updateUser
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+        .get(getUsers) 
+        .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+        .get(getSingleUser)
+        .delete(deleteUser)
+        .put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+        .post(addFriend)
+        .delete(removeFriend);
+
+module.exports = router;
